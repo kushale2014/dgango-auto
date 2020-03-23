d706e78e79967bd7f7e57c367f7114d60d995df0
Alex <kushale2014@gmail.com>
2020-03-23 12:44:51 +0200

    1111

--- mainapp/get_olx.py
+++ mainapp/get_olx.py
@@ -5,14 +5,14 @@ session = requests.Session()
 headers = {'accept' : '*/*',
         'user-agent' : 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
         }
-base_url = 'https://www.olx.ua/transport/legkovye-avtomobili/od/?search%5Bfilter_float_price%3Ato%5D=1000&search%5Bfilter_float_motor_year%3Afrom%5D=1995&search%5Bphotos%5D=1&page={page}&currency=USD'
+base_url = 'https://www.olx.ua/transport/legkovye-avtomobili/od/?search%5Bfilter_float_price%3Ato%5D=1200&search%5Bfilter_float_motor_year%3Afrom%5D=1995&search%5Bphotos%5D=1&page={page}&currency=USD'
 cars = []
 
 def get_html(url):
     res = session.get(url, headers=headers)
     return res.text
 
-def get_total_pages(soup):
+def get_count_pages(soup):
     pages = soup.find('div', attrs={'class':'pager rel clr'}).find_all('span', attrs={'class':'item fleft'})
     count = pages[-1].text.strip()
     return int(count)
@@ -27,28 +27,31 @@ def get_parcing(soup):
         title = ss.find('a', attrs={'data-cy':'listing-ad-title'}).text.strip()
         href = ss.find('a', attrs={'data-cy':'listing-ad-title'}).get('href')
         price_S = ss.find('p', attrs={'class':'price'}).text.strip()
-        # price_U = ss.find('span', attrs={'data-currency':'UAH'}).text
-        # definition_data = ss.find('div', attrs={'class':'definition-data'})
+        place_and_date = ss.find('td', attrs={'class':'bottom-cell'}).text.strip()
+        pp = place_and_date.split()
+        place = ' '.join(pp[:-2])
+        date = ' '.join(pp[-2:])
         cars.append({
             'img' : img,
             'title' : title,
             'href' : href,
             'price_S' : price_S,
-            # 'prics_U' : price_U,
-            # 'definition_data' : definition_data
+            'place' : place,
+            'date' : date,
         })
 
-def get_cars_olx():
+def get_cars_olx(count_cars):
     cars.clear()
     html = get_html(base_url.format(page=1))
     soup = bs(html, 'lxml')
-    get_parcing(soup)
-    # count = get_total_pages(soup)
-    # for i in range(1, count+1) :
-    #     html = get_html(base_url.format(page=i))
-    #     soup = bs(html, 'lxml')
-    #     get_parcing(soup)
-    return cars
+    count_pages = get_count_pages(soup)
+    page = 1
+    while len(cars) < count_cars and page <= count_pages :
+        html = get_html(base_url.format(page=page))
+        soup = bs(html, 'lxml')
+        get_parcing(soup)
+        page += 1
+    return cars[:count_cars]
 
 if __name__ == '__main__':
     cars = get_cars_olx()
--- mainapp/views.py
+++ mainapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from mainapp.get_olx import get_cars_olx
 
 def index(request):
-    cars = get_cars_olx()
-    return render(request, 'mainapp/index.html', {'cars' : cars})
-    # return render(request, 'index.html', {'all_cars':all_cars})
-
+    cars = get_cars_olx(100)
+    cars0 = [cars[key] for key in range(0, len(cars), 2)]
+    cars1 = [cars[key] for key in range(1, len(cars)+1, 2)]
+    return render(request, 'mainapp/index.html', {'cars0' : cars0, 'cars1' : cars1})
+    
