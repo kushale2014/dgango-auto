5e019db07e2cdd499e65606bea67132a01afc777
Alex <kushale2014@gmail.com>
2020-03-23 16:35:40 +0200

    templatetags

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,7 +3,7 @@ from mainapp.get_olx import get_cars_olx
 
 def index(request):
     cars = get_cars_olx(100)
-    cars0 = [cars[key] for key in range(0, len(cars), 2)]
-    cars1 = [cars[key] for key in range(1, len(cars)+1, 2)]
-    return render(request, 'mainapp/index.html', {'cars0' : cars0, 'cars1' : cars1})
+    # cars0 = [cars[key] for key in range(0, len(cars), 2)]
+    # cars1 = [cars[key] for key in range(1, len(cars)+1, 2)]
+    return render(request, 'mainapp/index.html', {'cars' : cars})
     
